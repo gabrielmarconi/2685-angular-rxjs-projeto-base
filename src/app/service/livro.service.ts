5e41f407d1972cb71ae13690b0a8cf4fd858f2d8
gabrielmarconi <55216954+gabrielmarconi@users.noreply.github.com>
2025-08-12 22:49:18 -0300

    Commit pra otimizacao de busca

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
+import { Item, LivrosResultado } from '../models/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +13,11 @@ export class LivroService {
     private http: HttpClient
   ) { }
 
-  buscar(valorDigitado: string): Observable<any> {    
+  buscar(valorDigitado: string): Observable<Item[]> {    
     const params = new HttpParams().append('q', valorDigitado)
-    return this.http.get(this.API, { params })
+    return this.http.get<LivrosResultado>(this.API, { params }).pipe(
+      tap((retornoApi) => console.log(retornoApi)),
+      map(resultado => resultado.items)
+    )
   }
 }
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,27 +1,37 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, filter, map, switchMap } from 'rxjs';
+import { Item } from 'src/app/models/interfaces';
+import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
+const PAUSA = 300;
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
 export class ListaLivrosComponent {
-
-  listaLivros: [];
-  campoBusca: string = ''
+  
+  campoBusca = new FormControl();    
 
   constructor(
     private service: LivroService
   ) { }
 
-  buscarLivros() {
-    this.service.buscar(this.campoBusca).subscribe(
-      (retornoAPI) => {
+  livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
+    filter((valorDigitado) => valorDigitado.length >= 3),
+    distinctUntilChanged(),
+    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+    map((items) => this.livrosResultadoParaLivros(items))
+  )
 
-      }
-    )
-  }
+  livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
+    return items.map(item => {
+      return new LivroVolumeInfo(item)
+    })
+  }  
 }
 
 
